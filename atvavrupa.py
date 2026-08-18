ad18bfa9761abeb2e0ddb1b183f863c1c47b263c
bnyusuf67-crypto <bnyusuf67@gmail.com>
2026-08-18 19:23:01 +0300

    Update atvavrupa.py

--- atvavrupa.py
+++ atvavrupa.py
@@ -7,7 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 STREAM_DIR = "streams"
 M3U8_FILENAME = os.path.join(STREAM_DIR, "atvavrupa.m3u8")
 SEQUENCE_FILE = os.path.join(STREAM_DIR, "sequence.txt")
-MAX_SEGMENTS = 150
+MAX_SEGMENTS = 160
 
 BASE_URL = "https://bnyusuf67-crypto.github.io/streams/"
 
