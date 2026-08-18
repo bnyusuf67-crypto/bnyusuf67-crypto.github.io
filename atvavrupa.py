eea9c45f5e29eee7f10b7dc60087d5b3a99f4b9e
bnyusuf67-crypto <bnyusuf67@gmail.com>
2026-08-18 16:02:40 +0300

    Update atvavrupa.py

--- atvavrupa.py
+++ atvavrupa.py
@@ -155,7 +155,7 @@ def main():
     
     for ts_file in existing_ts:
         m3u8_content += "#EXTINF:6.000,\n"
-        m3u8_content += f"{ts_file}\n"
+        m3u8_content += f"https://bnyusuf67-crypto.github.io/{ts_file}\n"
 
     with open(m3u8_path, "w", encoding="utf-8") as f:
         f.write(m3u8_content)
